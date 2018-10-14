d28d53574a6e3b9230a13624b831bc4aa125a052
Ariel <pzhen001@g.ucla.edu>
2018-10-13 17:05:13 -0700

    Do migrations

--- db/migrate/20181013235347_create_users.rb
+++ db/migrate/20181013235347_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+      t.integer :income
+      t.string :uid
+      t.string :email
+      t.string :image
+      t.integer :dci_number
     end
   end
 end
--- db/migrate/20181013235624_create_transactions.rb
+++ db/migrate/20181013235624_create_transactions.rb
@@ -1,7 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-
+      t.string :description
+      t.integer :amount
+      t.integer :user_id
+      t.integer :category_id
       t.timestamps
     end
   end
