5bd8dac454c2772928d43c80a4571b9988d8bbcb
Ariel <pzhen001@g.ucla.edu>
2018-10-13 18:27:11 -0700

    set up users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
+
   def index
-  end 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def show
+    return head(:forbidden) unless session.include? :user_id
+  end
+
+  def edit
+    return head(:forbidden) unless session.include? :user_id
+  end
+
+  private
+
+    def set_user
+      @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :password,
+        :password_confirmation
+      )
+    end
 end
