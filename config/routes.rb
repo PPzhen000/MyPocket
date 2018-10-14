26621660eea82e1bfb2b028107c979e3f99fa37e
Ariel <pzhen001@g.ucla.edu>
2018-10-13 17:08:43 -0700

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :categories
+  resources :transactions
 
+  resources :categories do
+    resources :transactions
+  end
+
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  get "/auth/facebook/callback", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+
+  root 'users#index'
 end
