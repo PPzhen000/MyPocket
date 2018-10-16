1ab9ff79a13ab5b42dc1e7ecefd63ee77067168f
Ariel <pzhen001@g.ucla.edu>
2018-10-16 00:09:36 -0700

    refactor user's expense view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,22 @@
 class Category < ApplicationRecord
   has_many :transactions
   has_many :users, through: :transactions
+
+  def category_total
+     self.transactions.sum(:amount)
+  end
+
+  def status
+    if self.monthly_budget && self.category_total
+      self.monthly_budget >= self.category_total ? "doing good" : "not so great"
+    end
+  end
+
+  def total_spent
+    total = 0
+    self.each do |ca|
+      total += ca.category_total
+    end
+    total
+  end
 end
