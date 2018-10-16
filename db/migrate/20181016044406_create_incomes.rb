1dbdc184084996c6c56205a28ff934ec08a3477b
Ariel <pzhen001@g.ucla.edu>
2018-10-15 21:48:38 -0700

    create income table

--- db/migrate/20181016044406_create_incomes.rb
+++ db/migrate/20181016044406_create_incomes.rb
@@ -1,7 +1,8 @@
 class CreateIncomes < ActiveRecord::Migration[5.2]
   def change
-    create_table :incomes do |t|
-
+    create_table :incomes do |t|   
+      t.integer :amount
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_042332) do
+ActiveRecord::Schema.define(version: 2018_10_16_044406) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "incomes", force: :cascade do |t|
+    t.integer "amount"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
