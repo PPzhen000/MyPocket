53d341fdfb4ae67a63dcee604c5c055a42c6ffa5
Ariel <pzhen001@g.ucla.edu>
2018-10-15 23:23:11 -0700

    add edit category form for users to update monthly budget for each category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,25 @@
 class CategoriesController < ApplicationController
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    # raise params.inspect
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    if @category.save
+      flash[:success] = "Monthly Budget was updated"
+    else
+      flash[:error] = "Monthly Budget could not be updated"
+    end
+    redirect_to current_user
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :monthly_budget)
+  end
+
 end
