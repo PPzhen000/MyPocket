64cea13aae26103c6a5068fcdd1edcf3fe793efd
Ariel <pzhen001@g.ucla.edu>
2018-10-22 23:57:01 -0700

    add scope methods for transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,6 +7,7 @@ class Transaction < ApplicationRecord
     message: "You already input this activity" }
   validates :amount, numericality: { only_integer: true }
 
-  scope :most_recent, -> { order("created_at desc")}
-
+  scope :most_recent, -> { order("created_at DESC")}
+  scope :by_price, -> { order("amount DESC")}
+  scope :by_category, -> (category_id) { where category_id: category_id }
 end
