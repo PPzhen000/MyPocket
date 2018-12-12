b2c40cdfd74a040af0864e52179194c0d9a228c6
Ariel <pzhen001@g.ucla.edu>
2018-12-12 14:31:36 -0800

    create $.ready() function for transaction

---
+++ app/assets/javascripts/transaction.js
@@ -0,0 +1,11 @@
+$(document).on('turbolinks:load', function () {
+  $(".expense").on('click', function(e) {
+    e.preventDefault();
+    loadIncome();
+  });
+});
+
+function loadIncome() {
+  
+
+}
