17b6867a089eb69f361ba81d9bc0e1bb2c033e0f
Ariel <pzhen001@g.ucla.edu>
2018-12-14 23:39:45 -0800

    add loadSavingGoal() function to document ready

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   loadExpenses();
   loadIncome();
+  loadSavingGoal()
 }); // end of document ready
 
 function loadExpenses(){
@@ -34,34 +35,34 @@ class Expense {
   //
   // }
 } //end of expense constructor
-
-function loadIncome(){
-  $("a.inc_title").on('click', function(e){
-    $.get(this.href, function(data) {
-      let arrayOfLi = [];
-      data.forEach(function(income) {
-        let response = new Income(income);
-        arrayOfLi.push(formatIncome(response))
-      });
-      let fullHTML = `<ul>${arrayOfLi.join('')}</ul>`
-      $("#income_list").append(fullHTML)
-    });
-    e.preventDefault();
-  });
-}
-
-class Income {
- constructor(attributes){
-  this.id = attributes.id;
-  this.description = attributes.description;
-  this.amount = attributes.amount;
- }
-}
+//
+// function loadIncome(){
+//   $("a.inc_title").on('click', function(e){
+//     $.get(this.href, function(data) {
+//       let arrayOfLi = [];
+//       data.forEach(function(income) {
+//         let response = new Income(income);
+//         arrayOfLi.push(formatIncome(response))
+//       });
+//       let fullHTML = `<ul>${arrayOfLi.join('')}</ul>`
+//       $("#income_list").append(fullHTML)
+//     });
+//     e.preventDefault();
+//   });
+// }
+//
+// class Income {
+//  constructor(attributes){
+//   this.id = attributes.id;
+//   this.description = attributes.description;
+//   this.amount = attributes.amount;
+//  }
+// }
 
 function formatExpense(response){
   return `<li>${response.description}  -  ${response.category}  -  ${response.amount}</li>`
 }
 
-function formatIncome(response){
-  return `<li>${response.description} - ${response.amount}</li>`
-}
+// function formatIncome(response){
+//   return `<li>${response.description} - ${response.amount}</li>`
+// }
