1d42fa06dafdbfe05095ee495dc6016564ad9380
Ariel <pzhen001@g.ucla.edu>
2018-12-17 14:29:52 -0800

    move user outside of doc ready

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,25 +1,25 @@
+let user = {
+  data: {
+    income: [],
+    expense: [],
+    saving_goal: []
+  },
+  total: {
+    income: 0,
+    expense: 0,
+    saving_goal: 0
+  }
+};
+
 $(document).ready(function(){
-  let user = {
-    data: {
-      income: [],
-      expense: [],
-      saving_goal: []
-    },
-    total: {
-      income: 0,
-      expense: 0,
-      saving_goal: 0
-    }
-    budget: 0
-  };
 
   loadIncome();
   loadExpense();
   loadSavingGoal();
   createIncome();
-  createExpense();
-  createSavingGoal();
-  deleteIncome();
-  deleteExpense();
-  deleteSavingGoal();
+  // createExpense();
+  // createSavingGoal();
+  // deleteIncome();
+  // deleteExpense();
+  // deleteSavingGoal();
 });
